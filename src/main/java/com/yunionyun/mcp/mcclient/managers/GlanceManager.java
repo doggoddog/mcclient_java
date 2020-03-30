6f5c84de2bd1949156b32d52fc428cdb0ffcb5c2
zhaoxiangchun <zhaoxiangchun@yunion.cn>
2020-03-30 13:40:05 +0800

    振华定制化mcclient功能

--- src/main/java/com/yunionyun/mcp/mcclient/managers/GlanceManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/GlanceManager.java
@@ -6,4 +6,8 @@ public class GlanceManager extends ResourceManager {
 	public GlanceManager(String keyword, String keywordPlural, String[] columns, String[] adminColumns) {
 		super("image", EndpointType.InternalURL, "v1", columns, adminColumns, keyword, keywordPlural, null);
 	}
+
+	public GlanceManager(String keyword, String keywordPlural, EndpointType endpointType,String[] columns, String[] adminColumns) {
+		super("image", endpointType, "v1", columns, adminColumns, keyword, keywordPlural, null);
+	}
 }
--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/ImageManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/ImageManager.java
@@ -1,8 +1,7 @@
 package com.yunionyun.mcp.mcclient.managers.impl;
 
-import java.io.IOException;
-
 import com.alibaba.fastjson.JSONObject;
+import com.yunionyun.mcp.mcclient.EndpointType;
 import com.yunionyun.mcp.mcclient.JSONClientException;
 import com.yunionyun.mcp.mcclient.Session;
 import com.yunionyun.mcp.mcclient.Utils;
@@ -10,18 +9,31 @@ import com.yunionyun.mcp.mcclient.common.McClientJavaBizException;
 import com.yunionyun.mcp.mcclient.managers.GlanceManager;
 import com.yunionyun.mcp.mcclient.managers.ListResult;
 
+import java.io.IOException;
+
 public class ImageManager extends GlanceManager {
 	public ImageManager() {
 		super("image", "images",
-                new String[] {"ID", "Name", "Tags", "Disk_format",
-            "Size", "Is_public", "OS_Type",
-            "OS_Distribution", "OS_version",
-            "Min_disk", "Min_ram", "Status",
-            "Notes", "OS_arch", "Preference",
-            "OS_Codename", "Parent_id"},
-                new String[] {"Owner", "Owner_name"});
+				new String[]{"ID", "Name", "Tags", "Disk_format",
+						"Size", "Is_public", "OS_Type",
+						"OS_Distribution", "OS_version",
+						"Min_disk", "Min_ram", "Status",
+						"Notes", "OS_arch", "Preference",
+						"OS_Codename", "Parent_id"},
+				new String[]{"Owner", "Owner_name"});
 	}
-	
+
+	public ImageManager(EndpointType endpointType) {
+		super("image", "images", endpointType,
+				new String[]{"ID", "Name", "Tags", "Disk_format",
+						"Size", "Is_public", "OS_Type",
+						"OS_Distribution", "OS_version",
+						"Min_disk", "Min_ram", "Status",
+						"Notes", "OS_arch", "Preference",
+						"OS_Codename", "Parent_id"},
+				new String[]{"Owner", "Owner_name"});
+	}
+
 	public ListResult List(Session s, JSONObject query) throws McClientJavaBizException, IOException, JSONClientException {
 		StringBuilder url = new StringBuilder();
 		url.append("/");
